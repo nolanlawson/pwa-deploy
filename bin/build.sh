6d6c5648ddac4fb3a8c93c37206a3bfb577f9541
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-28 21:35:59 -0400

    initial commit

---
+++ bin/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ -z ~/.keystore_config ]; then
+  echo "couldn't find keystore config!"
+  echo "you need a ~.keystore_config file with:"
+  echo "export ANDROID_KEYSTORE=keystore"
+  echo "export ANDROID_STORE_PASSWORD=pass"
+  echo "export ANDROID_ALIAS=name"
+  echo "export ANDROID_PASSWORD=pass"
+  exit 1
+fi
+
+source ~/.keystore_config
+
+cd android
+./gradlew assembleRelease
